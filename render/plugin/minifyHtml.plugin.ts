7df6ccd2cd755609973bb8e20f97bcc0700c91a6
Jakub Flaska <jflaska@baosystems.com>
2024-03-12 12:35:43 -0400

    cleaning

--- render/plugin/minifyHtml.plugin.ts
+++ render/plugin/minifyHtml.plugin.ts
@@ -2,7 +2,6 @@ export default function minifyHtmlPlugin() {
     return {
         name: 'transform-file',
         transform(src, id) {
-            console.log(id)
             if (id.includes('css')) return {
                 code: src.replace(/\s|\n/gm, '').replace(/\/\*.+?\*\//g,'')
             }
